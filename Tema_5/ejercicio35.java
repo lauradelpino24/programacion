c258c683a7d8106e3e68e48234cf2b0d32c5bb70
lauradelpino24 <lauradelpinoheredia20@gmail.com>
2018-10-18 10:02:01 +0200

    35 actualizado

--- Tema_5/ejercicio35.java
+++ Tema_5/ejercicio35.java
@@ -21,38 +21,58 @@ public class ejercicio35 {
     System.out.print("Introduzca la altura de la X: ");
     int altura = Integer.parseInt(s.nextLine());
     
-    int patas = altura / 2;
-    String espaciosInternos = "";
-    String espacios = "";
-    
-    for (int i = 1; i < (altura - 1); i++) { // Configurar espacios internos
+    if (altura < 3 || altura % 2 == 0) {
       
-      espaciosInternos += " ";
-    }
+      System.out.print("Datos incorrectos, se debe introducir una ");
+      System.out.print("altura impar y mayor o igual a 3 ");
+      
+    } else {
     
-      for (int i = 0; i < patas; i++) { // Mitad superior de la X
+      int patas = altura / 2; // Define el largo de las mitades de la X
+      
+      String espaciosInternos = ""; // Almacena los espacios internos
+      int espaciosIntLongitud = 0; // Almacena cuántos espacios internos hay
+      
+      String espacios = ""; // Almacena los espacios
+      int espaciosLongitud = 0; // Almacena cuántos espacios hay
+      
+      for (int i = 1; i < (altura - 1); i++) { // Configurar espacios internos
         
+        espaciosInternos += " ";
+        espaciosIntLongitud ++;
+      }
+      
+      for (int i = 0; i < patas; i++) { // Mitad superior de la X
+        // Debemos parar una iteración antes para no pasarnos
+        if (i < (patas - 1)) {
+            
+          espaciosIntLongitud -= 2;
+        }
+        // Vamos pintando la parte superior línea a línea
         System.out.print(espacios);
         System.out.print("*");
         System.out.print(espaciosInternos);
         System.out.print("*\n");
-        
-        espaciosInternos = espaciosInternos.substring(i, (patas - 1));
+        // Acortamos los espacios internos y añadimos espacios
+        espaciosInternos = espaciosInternos.substring(0, espaciosIntLongitud);
         espacios += " ";
+        espaciosLongitud ++;
       }
+        
+      System.out.println(espacios + "*"); // Asterisco central
       
-    System.out.println(espacios + "*"); // Asterisco central
-    
-    for (int i = 0; i < patas; i++) { // Mitad inferior de la X
-      
-      espacios = espacios.substring(i, (patas - 1));
-      espaciosInternos += " ";
-      
-      System.out.print(espacios);
-      System.out.print("*");
-      System.out.print("*\n");
+      for (int i = 0; i < patas; i++) { // Mitad inferior de la X
+        // Acortamos espacios
+        espaciosLongitud -= 1;      
+        espacios = espacios.substring(0, espaciosLongitud);
+        // Pintamos
+        System.out.print(espacios);
+        System.out.print("*");
+        System.out.print(espaciosInternos);
+        System.out.print("*\n");
+        // Añadimos espacios internos
+        espaciosInternos += " " + " ";
+      }
     }
-    
-  System.out.println();
  }
 }
