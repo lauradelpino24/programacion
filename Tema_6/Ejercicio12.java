d691d9e9d8a02db041bfdc11ffb4ffac7bfe3068
lauradelpino24 <lauradelpinoheredia20@gmail.com>
2018-11-06 08:50:43 +0100

    12-14

---
+++ Tema_6/Ejercicio12.java
@@ -0,0 +1,21 @@
+/*
+ * Ejercicio 12 - Tema 6
+ * Realiza un programa que llene la pantalla de caracteres aleatorios (a lo Matrix)
+ * con el código ascii entre el 32 y el 126. Puedes hacer casting con (char) para
+ * convertir un entero en un carácter.
+ */
+package tema6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author laura
+ */
+public class Ejercicio12 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    
+  }
+}
---
+++ Tema_6/Ejercicio13.java
@@ -0,0 +1,27 @@
+/*
+ * Ejercicio 13 - Tema 6
+ * Escribe un programa que simule la tirada de dos dados. El programa deberá
+ * continuar tirando los dados una y otra vez hasta que en alguna tirada los dos
+ * dados tengan el mismo valor.
+ */
+package tema6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author laura
+ */
+public class Ejercicio13 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    int dado1, dado2;
+    
+    do {
+      dado1 = (int)(Math.random() * 6) + 1;
+      dado2 = (int)(Math.random() * 6) + 1;
+      System.out.println(dado1 + " " + dado2);
+    } while (dado1 != dado2);
+  }
+}
