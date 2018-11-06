210d131c4f735c2c1ca47ee9a97a1865b24b8a19
lauradelpino24 <lauradelpinoheredia20@gmail.com>
2018-11-06 12:33:17 +0100

    12 actualizado

--- Tema_6/Ejercicio12.java
+++ Tema_6/Ejercicio12.java
@@ -13,9 +13,20 @@ import java.util.Scanner;
  * @author laura
  */
 public class Ejercicio12 {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     Scanner s = new Scanner(System.in);
+
+    int linea = 0;
     
-    
+    for(int i = 0; i < 8000; i++) {
+      System.out.print((char)(Math.random() * (126 - 32 + 1) + 32));
+      
+      if (linea++ == 60) {
+        linea = 0;
+        Thread.sleep(50);
+        System.out.println();
+      }
+    }
   }
+ }
 }
